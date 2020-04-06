ffc0f313f9b83f40465f6ded5a76d6b6cd6600ff
ILEM MARTINS <63267697+ilemjrj@users.noreply.github.com>
2020-04-06 20:41:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,28 +11,28 @@ Resposta = int(input(" Digite sua escolha: "))
 if Resposta == 1:
     
     print ("PERGUNTA NUMERO 1")
-    print ("Quantos ovos são necessaris para se fazer um bolo? \n Alternativa 1: 3 Ovos \n Alternativa 2: 5 Ovos \n Alternativa 3: 10 Ovos")
+    print ("Quantos ovos são necessários para se fazer um bolo? \n Alternativa 1: 3 Ovos \n Alternativa 2: 5 Ovos \n Alternativa 3: 10 Ovos")
     questao1 = int(input("Digite sua resposta: "))
     if questao1 != 2:
           break
     print (" Resposta errada ")
-    print (" Para fazer um bolo são necessarios 5 Ovos par qie ele possa ficar consistente")
+    print (" Para fazer um bolo são necessários 5 ovos para que ele possa ficar consistente")
          
     else:
         print (" Certa resposta ")
-        print ("Para fazer um bolo são necessarios 5 Ovos par qie ele possa ficar consistente")
+        print ("Para fazer um bolo são necessários 5 Ovos para que ele possa ficar consistente")
         Reais += 1
         
     print ("PERGUNTA NUMERO 2")
-    print ("Quantas xicaras de xa de arroz são necessarias para se fazer um bolo? \n Alternativa 1: 3 Xicaras \n Alternativa 2: 5 Xicaras \n Alternativa 3: 10 Xicaras")
+    print ("Quantas xícaras de arroz são necessárias para se fazer um bolo? \n Alternativa 1: 3 Xícaras \n Alternativa 2: 5 Xícaras \n Alternativa 3: 10 Xícaras")
     questao2 = int(input("Digite sua resposta: "))
     if questao2 != 1:
         print (" Resposta errada")
-        print ("Para fazer um bolo são necessarios  3 Xicaras par qie ele possa ficar consistente")
+        print ("Para fazer um bolo são necessárias  3 Xícaras para que ele possa ficar consistente")
         Reais +=  1
     else:
         print (" Certa resposta")
-        print ("Para fazer um bolo são necessarios 3 Xicaras par qie ele possa ficar consistente")
+        print ("Para fazer um bolo são necessárias 3 Xícaras para que ele possa ficar consistente")
         
-    print ("Voce possui até o momento R$ %2.8f" % (Reais))
+    print ("Você possui até o momento R$ %2.8f" % (Reais))
     
